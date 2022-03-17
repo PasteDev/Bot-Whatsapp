2c5ae9227ceccc4f3b94254f85e5e5359e1ba25d
Pastelito_ <42726698+PasteDev@users.noreply.github.com>
2022-03-17 14:15:37 -0300

    Add files via upload

---
+++ Bot-Whatsapp.js
@@ -0,0 +1,116 @@
+require('dotenv').config()
+const qrcode = require("qrcode-terminal");
+const { Client, MessageMedia } = require("whatsapp-web.js");
+const libre = require('libreoffice-convert');
+libre.convertAsync = require('util').promisify(libre.convert);
+
+
+const country_code = process.env.COUNTRY_CODE;
+const number = process.env.NUMBER;
+const msg = process.env.MSG;
+
+const client = new Client({ puppeteer: { headless: true } });
+
+
+client.on("qr", qr => {
+    qrcode.generate(qr, { small: true });
+});
+
+client.on("authenticated", () => {
+    console.log('Bot -> Autenticado')
+});
+
+client.on("auth_failure", msg => {
+    console.log('Bot -> Autentificación Fallida', msg);
+})
+
+
+client.on("ready", () => {
+    console.log("Bot -> El cliente está activo");
+    let chatId = country_code + number + "@c.us";
+    client.sendMessage(chatId, msg)
+        .then(response => {
+            if (response.id.fromMe) {
+                console.log("Bot -> Estoy funcionando correctamente!");
+            }
+        })
+});
+client.on("message_create", async message => {
+    if (!message.fromMe) {
+        if (message.hasMedia) {
+            const downloaded = await message.downloadMedia()
+            const buffer = Buffer.from(downloaded.data, "base64");
+            switch (downloaded.mimetype) {
+                case "application/vnd.openxmlformats-officedocument.wordprocessingml.document":
+                case "application/msword":
+                case "application/vnd.openxmlformats-officedocument.presentationml.presentation":
+                case "application/vnd.ms-powerpoint":
+                case "application/vnd.ms-excel":
+                case "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet": {
+                    const tosend = await libre.convertAsync(buffer, ".pdf", undefined)
+                    const att = new MessageMedia("application/pdf", tosend.toString("base64"), downloaded.filename.split(".").slice(0, -1).join("."))
+                    message.reply(att);
+                    const contact = await message.getContact();
+                    const CodeCountry = await contact.getCountryCode();
+                    const formattedNumber = await contact.getFormattedNumber()
+                    const nationalPhoneCode = require('national-phone-code')
+                    const country = nationalPhoneCode.getCodeInfo(CodeCountry).english_name
+                    const timestamp = require('moment-timezone');
+                    console.log("[" + timestamp.tz("America/Argentina/Buenos_Aires").format("DD/MM - HH:mm") + "] [" + country + "] (" + contact.pushname + " | " + formattedNumber + ") mandó un Archivo para convertirlo en PDF");
+                    break;
+                }
+                case 'image/jpeg': {
+                    const sharp = require('sharp');
+                    const bufferImage = await sharp(buffer)
+                        .resize(500, 500)
+                        .webp()
+                        .toBuffer()
+                    const base64Image = bufferImage.toString("base64");
+                    const messageMedia = new MessageMedia("image/webp", base64Image, "sticker.webp");
+
+                    await message.reply(messageMedia, null, { sendMediaAsSticker: true, stickerAuthor: '+543644178536', stickerName: 'Whatsapp Bot' });
+                    const contact = await message.getContact();
+                    const CodeCountry = await contact.getCountryCode();
+                    const formattedNumber = await contact.getFormattedNumber()
+                    const nationalPhoneCode = require('national-phone-code')
+                    const country = nationalPhoneCode.getCodeInfo(CodeCountry).english_name
+                    const timestamp = require('moment-timezone');
+                    console.log("[" + timestamp.tz("America/Argentina/Buenos_Aires").format("DD/MM - HH:mm") + "] [" + country + "] (" + contact.pushname + " | " + formattedNumber + ") mandó una Imagen para convertirla en Sticker");
+                    break;
+                }
+                case 'video/mp4': {
+                    await message.reply(downloaded, null, { sendMediaAsSticker: true, stickerAuthor: '+543644178536', stickerName: 'Whatsapp Bot' });
+                    const contact = await message.getContact();
+                    const CodeCountry = await contact.getCountryCode();
+                    const formattedNumber = await contact.getFormattedNumber()
+                    const nationalPhoneCode = require('national-phone-code')
+                    const country = nationalPhoneCode.getCodeInfo(CodeCountry).english_name
+                    const timestamp = require('moment-timezone');
+                    console.log("[" + timestamp.tz("America/Argentina/Buenos_Aires").format("DD/MM - HH:mm") + "] [" + country + "] (" + contact.pushname + " | " + formattedNumber + ") mandó un Gif/Video para convertirlo en Sticker Animado");
+                    break;
+                }
+            }
+        }
+    }
+
+});
+
+
+client.on('group_join', async message => {
+    const chat = await message.getChat();
+    const foto = "https://c.tenor.com/Or_pMDyHdygAAAAd/no-no-no-nope.gif"
+    const media = await MessageMedia.fromUrl(foto);
+    await chat.sendMessage(media, { caption: '⚠️| *No pueden agregarme a Grupos!*\n\n_Si deseas usarme mandame un mensaje por_ *PRIVADO*' })
+    await new Promise((s) => setTimeout(s, 2000))
+    await chat.leave();
+    const contact = await message.getContact();
+    const CodeCountry = await contact.getCountryCode();
+    const formattedNumber = await contact.getFormattedNumber()
+    const nationalPhoneCode = require('national-phone-code')
+    const country = nationalPhoneCode.getCodeInfo(CodeCountry).english_name
+    const groupname = chat.name
+    const timestamp = require('moment-timezone');
+    console.log("[" + timestamp.tz("America/Argentina/Buenos_Aires").format("DD/MM - HH:mm") + "] [" + country + "] (" + contact.pushname + " | " + formattedNumber + ") intentó agregarme al grupo " + groupname + " pero no pudo.");
+});
+
+client.initialize();
